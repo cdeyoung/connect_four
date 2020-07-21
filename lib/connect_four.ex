a1129a9b676ff25261305b8f627aae074c01513a
Clinton <clinton@axisavant.com>
2020-07-21 15:23:18 -0600

    Contexts
    Add gameplay functions to the context.

--- lib/connect_four.ex
+++ lib/connect_four.ex
@@ -2,17 +2,44 @@ defmodule ConnectFour do
   @moduledoc """
   Documentation for `ConnectFour`.
   """
+  import ConnectFour.Guards
+  alias ConnectFour.Game
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ConnectFour.hello()
-      :world
-
+  Get the current board.
   """
-  def hello do
-    :world
+  def board do
+    Game.board()
+  end
+
+  @doc """
+  Reset the Connect Four game.
+  """
+  def reset do
+    Game.reset()
+  end
+
+  @doc """
+  Take a turn.
+  """
+  def take_turn(column, player) when valid_turn(column, player) do
+    if whose_turn() == player do
+      Game.take_turn(column, player)
+    else
+      IO.puts("This would have been a superlative choice if it were actually your turn.")
+      :not_your_turn
+    end
+  end
+
+  def take_turn(_column, _player) do
+    IO.puts("You just dropped your piece on the floor. Try aiming for the board next time.")
+    :not_your_turn
+  end
+
+  @doc """
+  Get the current player.
+  """
+  def whose_turn do
+    Game.whose_turn()
   end
 end
