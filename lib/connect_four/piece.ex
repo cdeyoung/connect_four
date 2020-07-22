96dde84141380d8a44d58b33417fc1e0999d6b97
Clinton <clinton@axisavant.com>
2020-07-22 00:48:00 -0600

    GenServer
    Finish the functionality of the Piece GenServer.

--- lib/connect_four/piece.ex
+++ lib/connect_four/piece.ex
@@ -11,8 +11,8 @@ defmodule ConnectFour.Piece do
   @doc """
   Start the ConnectFour.Game server with the specified parameters.
   """
-  def start_link(args) do
-    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  def start_link(%{position: position} = args) do
+    GenServer.start_link(__MODULE__, args, name: {:global, "#{inspect position}"})
   end
 
   ################################################################################
@@ -21,10 +21,14 @@ defmodule ConnectFour.Piece do
 
   @impl GenServer
   def init(state) do
-    state = Map.put(state, :is_winner, _won?(state.position))
+    state = Map.put(state, :is_winner, _won?(state))
 
-    GenServer.call(__MODULE__, :check_for_win)
     {:ok, state}
+  end
+
+  @impl GenServer
+  def handle_call(:take_turn, _from, state) do
+    {:reply, state.is_winner, state}
   end
 
   @impl GenServer
@@ -32,23 +36,93 @@ defmodule ConnectFour.Piece do
     {:reply, state.player, state}
   end
 
+  @impl GenServer
+  def handle_cast(:stop, state) do
+    {:stop, :normal, state}
+  end
+
   ################################################################################
   # Private Functions.
   ################################################################################
 
-  defp _four_in_a_row?([{_, _,}, {_, _}, {_, _}, {_, _}]), do: true
+  defp _down_from_point(row, column), do: _down_from_point(row, column, 0, [{row, column}])
 
-  defp _four_in_a_row?([_, {_, _,}, {_, _}, {_, _}, {_, _}]), do: true
+  defp _down_from_point(5, _column, _count, acc), do: acc
 
-  defp _four_in_a_row?([{_, _,}, {_, _}, {_, _}, {_, _}, _]), do: true
+  defp _down_from_point(_row, 6, _count, acc), do: acc
 
-  defp _four_in_a_row?([{_, _,}, {_, _}, {_, _}, {_, _}, _, _]), do: true
+  defp _down_from_point(_row, _column, count, acc) when count > 3, do: acc
 
-  defp _four_in_a_row?([_, _, {_, _,}, {_, _}, {_, _}, {_, _}]), do: true
+  defp _down_from_point(row, column, count, acc) do
+    row = row + 1
+    column = column + 1
+    _down_from_point(row, column, count + 1, [ {row, column} | acc ])
+  end
+
+  defp _down_to_point(row, column), do: _down_to_point(row, column, 0, [{row, column}])
+
+  defp _down_to_point(0, _column, _count, acc), do: acc
+
+  defp _down_to_point(_row, 0, _count, acc), do: acc
+
+  defp _down_to_point(_row, _column, count, acc) when count > 3, do: acc
+
+  defp _down_to_point(row, column, count, acc) do
+    row = row - 1
+    column = column - 1
+    _down_to_point(row, column, count + 1, [ {row, column} | acc ])
+  end
+
+  defp _up_to_point(row, column), do: _up_to_point(row, column, 0, [{row, column}])
+
+  defp _up_to_point(5, _column, _count, acc), do: acc
+
+  defp _up_to_point(_row, 0, _count, acc), do: acc
+
+  defp _up_to_point(_row, _column, count, acc) when count > 3, do: acc
+
+  defp _up_to_point(row, column, count, acc) do
+    row = row + 1
+    column = column - 1
+    _up_to_point(row, column, count + 1, [ {row, column} | acc ])
+  end
+
+  defp _up_from_point(row, column), do: _up_from_point(row, column, 0, [{row, column}])
+
+  defp _up_from_point(0, _column, _count, acc), do: acc
+
+  defp _up_from_point(_row, 6, _count, acc), do: acc
+
+  defp _up_from_point(_row, _column, count, acc) when count > 3, do: acc
+
+  defp _up_from_point(row, column, count, acc) do
+    row = row - 1
+    column = column + 1
+    _up_from_point(row, column, count + 1, [ {row, column} | acc ])
+  end
+  defp _four_in_a_row?([:red, :red, :red, :red]), do: true
+
+  defp _four_in_a_row?([:yellow, :yellow, :yellow, :yellow]), do: true
+
+  defp _four_in_a_row?([_, :red, :red, :red, :red]), do: true
+
+  defp _four_in_a_row?([_, :yellow, :yellow, :yellow, :yellow]), do: true
+
+  defp _four_in_a_row?([:red, :red, :red, :red, _]), do: true
+
+  defp _four_in_a_row?([:yellow, :yellow, :yellow, :yellow, _]), do: true
+
+  defp _four_in_a_row?([:red, :red, :red, :red, _, _]), do: true
+
+  defp _four_in_a_row?([:yellow, :yellow, :yellow, :yellow, _, _]), do: true
+
+  defp _four_in_a_row?([_, _, :red, :red, :red, :red]), do: true
+
+  defp _four_in_a_row?([_, _, :yellow, :yellow, :yellow, :yellow]), do: true
 
   defp _four_in_a_row?(_), do: false
 
-  defp _line_check?(line) do
+  defp _line_check?(line, state) do
     if length(line) < 4 do
       false
     else
@@ -61,7 +135,11 @@ defmodule ConnectFour.Piece do
             [ nil | acc ]
 
           _ ->
-            [ GenServer.call({:global, "#{inspect pos}"}, :player) | acc ]
+            if (state.position == pos) do
+              [ state.player | acc ]
+            else
+              [ GenServer.call({:global, "#{inspect pos}"}, :player) | acc ]
+            end
         end
       end)
       |> Enum.sort()
@@ -69,28 +147,26 @@ defmodule ConnectFour.Piece do
     end
   end
 
-  defp _won?(position) do
-    surrounding = _surrounding_positions(position)
-    IO.inspect(surrounding)
-
-    _line_check?(surrounding.row_positions) or
-    _line_check?(surrounding.column_positions) or
-    _line_check?(surrounding.diagonal_down_positions) or
-    _line_check?(surrounding.diagonal_up_positions)
-  end
-
   defp _surrounding_positions({row, column}) do
-    x_beginning = if (row - 3 >= 0), do: row - 3, else: 0
-    x_end = if (row + 3 <= 6), do: row + 3, else: 6
-    y_beginning = if (column - 3 >= 0), do: column - 3, else: 0
-    y_end = if (column + 3 <= 5), do: column + 3, else: 5
-    diagonal_up_beginning = 6 - row
+    column_beginning = if (column - 3 >= 0), do: column - 3, else: 0
+    column_end = if (column + 3 <= 6), do: column + 3, else: 6
+    row_beginning = if (row - 3 >= 0), do: row - 3, else: 0
+    row_end = if (row + 3 <= 5), do: row + 3, else: 5
 
     %{
-      row_positions: Enum.reduce(x_beginning..x_end, [], fn(x, acc) -> [ {x, row} | acc ] end) |> Enum.sort(),
-      column_positions: Enum.reduce(y_beginning..y_end, [], fn(y, acc) -> [ {column, y} | acc ] end) |> Enum.sort(),
-      diagonal_down_positions: Enum.zip(x_beginning..x_end, y_beginning..y_end),
-      diagonal_up_positions: Enum.zip(diagonal_up_beginning..x_end, y_end..y_beginning)
+      row_positions: Enum.reduce(column_beginning..column_end, [], fn(x, acc) -> [ {row, x} | acc ] end) |> Enum.sort(),
+      column_positions: Enum.reduce(row_beginning..row_end, [], fn(y, acc) -> [ {y, column} | acc ] end) |> Enum.sort(),
+      diagonal_down_positions: Enum.sort(_down_to_point(row, column) ++ _down_from_point(row, column)) |> MapSet.new() |> Enum.to_list(),
+      diagonal_up_positions: Enum.sort(_up_to_point(row, column) ++ _up_from_point(row, column)) |> MapSet.new() |> Enum.to_list()
     }
+  end
+
+  defp _won?(state) do
+    surrounding = _surrounding_positions(state.position)
+
+    _line_check?(surrounding.row_positions, state) or
+    _line_check?(surrounding.column_positions, state) or
+    _line_check?(surrounding.diagonal_down_positions, state) or
+    _line_check?(surrounding.diagonal_up_positions, state)
   end
 end
