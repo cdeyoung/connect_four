48ccb18c0c97ea6ebd20903056246a5912fbbc12
Clinton <clinton@axisavant.com>
2020-07-22 00:46:12 -0600

    Contexts
    Add messages and retun checks.

--- lib/connect_four.ex
+++ lib/connect_four.ex
@@ -17,6 +17,7 @@ defmodule ConnectFour do
   """
   def reset do
     Game.reset()
+    IO.puts("I've reset the game for you so you can lose some more, if you'd like.")
   end
 
   @doc """
@@ -24,7 +25,25 @@ defmodule ConnectFour do
   """
   def take_turn(column, player) when valid_turn(column, player) do
     if whose_turn() == player do
-      Game.take_turn(column, player)
+      Game.take_turn(column - 1, player)
+      |> case do
+        :loser ->
+          IO.puts("DROPPED #{player} in #{column}.")
+          :loser
+
+        :out_of_bounds ->
+          IO.puts("No, no. You just go ahead and keep on trying until you find an open column.")
+          :out_of_bounds
+
+        :stalemate ->
+          IO.puts("Stalemate! Sigh...")
+          :stalemate
+
+        :winner ->
+          IO.puts("#{player |> Atom.to_string() |> String.upcase()} won!")
+          Game.reset()
+          :winner
+      end
     else
       IO.puts("This would have been a superlative choice if it were actually your turn.")
       :not_your_turn
@@ -32,8 +51,8 @@ defmodule ConnectFour do
   end
 
   def take_turn(_column, _player) do
-    IO.puts("You just dropped your piece on the floor. Try aiming for the board next time.")
-    :not_your_turn
+    IO.puts("You just dropped your piece on the floor, Butterthumbs. Try aiming for the board next time.")
+    :butter_thumbs
   end
 
   @doc """
